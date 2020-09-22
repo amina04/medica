de8ef204399bf15f692e16058bd28f4532d43ee4
amina04 <khachaamina0@gmail.com>
2020-09-22 16:53:12 +0200

    annuler les commentaires innutiles

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ List meds;
 List patient_list;
 List poche_list;
 List meds_detail;
-List debut_journee; 
+List debut_journee;
 List fin_journee;
 //=============
 List join_med;
@@ -61,44 +61,7 @@ int weight = 60;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var dbmanager = new Dbmedica();
-  //add med
-/*
-  int savemed = await dbmanager.insertMedicament(new Medicament("coly", 5, 6));
-  print('med saved $savemed');
-  int savemed2 =
-      await dbmanager.insertMedicament(new Medicament("rawn", 4, 56));
-  print('med saved $savemed2');*/
 
-  /*//get one med
-  Medicament pol = await dbmanager.getMed(6);
-  print('got med  ${pol.nom}');*/
-  /*//supprmer med
-  int meddeleted = await dbmanager.supprimerMed(1);
-  if (meddeleted == 1) {
-    print('the med is deleted');
-  }*/
-  //updating a medicament
-  /*Medicament oxydol = await dbmanager.getMed(8);
-  Medicament medUpdated = Medicament.fromMap({
-    "nom": "doliprane",
-    "qte_disponible": 23.3,
-    "volume_flacon": 4.1,
-    "id_medicament": 1,
-  });
-  await dbmanager.modifierMed(medUpdated);*/
-  //Get all meds
-/*
-   meds = await dbmanager.getAllMed();
-  for (int i = 0; i < meds.length; i++) {
-    //la liste contient des objet il faut cree les objets
-    // model var =model.map(liste)
-    Medicament med = Medicament.map(meds[i]);
-    print("medicament nom : ${med.nom}");
-    print("medicament qte  : ${med.qte_disponible}");
-    print("medicament volume : ${med.volume_flacon}");
-    print("medicament id: ${med.id_medicament}");
-  }
-*/
   int save1 = await dbmanager.insertPoches(new Poches(250));
   print('poche $save1');
   int save2 = await dbmanager.insertPoches(new Poches(500));
